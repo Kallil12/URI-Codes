55fc34ba22767d4c8950e6bb0046d5237797de52
kallil <kallilaraujo@gmail.com>
2019-01-21 12:03:15 -0300

    update

---
+++ 1185.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+char operacao;
+float M[12][12];
+float soma = 0;
+
+cin >> operacao;
+for(int i = 0; i < 12; i++){
+  for(int j = 0; j < 12; j++){
+    cin >> M[i][j];
+    if((i+j) < 11){
+      soma+=M[i][j];
+    }
+  }
+}
+
+if(operacao == 'S'){
+  cout << setprecision(1) << fixed << soma << endl;
+}
+else{
+  cout << setprecision(1) << fixed << soma/66.0 << endl;
+}
+
+
+
+
+  return 0;
+}
